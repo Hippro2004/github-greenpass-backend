84182b99e8879556d880dd65f16b1975d2635a60
Hippro2004 <chaiprach.2530@gmail.com>
2026-06-03 12:37:56 +0700

    📝 Add Cloudinary and UploadService

---
+++ src/main/java/com/example/greenpass/v1/services/UploadService.java
@@ -0,0 +1,26 @@
+package com.example.greenpass.v1.services;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UploadService {
+    private final Cloudinary cloudinary;
+
+    public String upload(MultipartFile file) throws IOException {
+        Map<String, Object> uploadResult = cloudinary.uploader().upload(
+                file.getBytes(),
+                ObjectUtils.emptyMap());
+        return uploadResult.get("secure_url").toString();
+    }
+
+}
