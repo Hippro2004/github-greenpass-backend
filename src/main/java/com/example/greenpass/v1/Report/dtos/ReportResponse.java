f2c9a494fcc6e0c48f04bf9a95eb6563e852e3ae
Hippro2004 <chaiprach.2530@gmail.com>
2026-09-04 05:15:44 +0700

    Update ReportResponse.java

--- src/main/java/com/example/greenpass/v1/Report/dtos/ReportResponse.java
+++ src/main/java/com/example/greenpass/v1/Report/dtos/ReportResponse.java
@@ -1,6 +1,7 @@
 package com.example.greenpass.v1.Report.dtos;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,10 +20,10 @@ public class ReportResponse {
     private String description;
     private String status;
     private LocalDate reportDate;
+    private LocalTime reportTime;
     private int parkId;
     private String parkName;
     private String username;
     private String image;
     private String parkRangerName;
 }
-
