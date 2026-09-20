a47f5e83942ab1b8a9c12ceffca7635c62976526
Hippro2004 <chaiprach.2530@gmail.com>
2026-09-20 23:11:50 +0700

    remove import

--- src/main/java/com/example/greenpass/v1/User/controller/UserController.java
+++ src/main/java/com/example/greenpass/v1/User/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.example.greenpass.v1.User.controller;
 
-import java.util.Map;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
